808716e5a08ac35602328e0dd15cfe87fa75cc1d
Anton Seminski <anton_siamianski@senla.eu>
2024-07-17 19:27:58 +0300

    2.0.19

--- src/services/auth-info.service.ts
+++ src/services/auth-info.service.ts
@@ -1,15 +1,15 @@
-import {JwtService} from '@nestjs/jwt';
 import {CODES} from '@buildery/error-codes';
 import {isHasEmpty} from '@buildery/nestjs-utils';
 import {throwException} from "@buildery/nest-exception-handling";
 import {EAuthInfo, IAuthInfo} from "../types";
 import {JwtTokenService} from "../jwt-token.service";
+import {jwtDecode} from "jwt-decode";
 
 /**
  * @description Service to handle Authorization header info
  * ***/
 export class AuthInfo {
-    public static async validate (request, authType, jwtService: JwtTokenService): Promise<IAuthInfo> {
+    public static async validate(request, authType, jwtService: JwtTokenService): Promise<IAuthInfo> {
         if (isHasEmpty(request, authType, jwtService)) throwException(CODES.AUTH.NO_AUTH_HEADER);
 
         const authHeader = request.headers?.authorization;
@@ -23,15 +23,15 @@ export class AuthInfo {
 
         const payload = await jwtService
             .verify(token)
-            .catch(error => throwException( CODES.AUTH.WRONG_CREDENTIALS, {reason: error.message} ));
+            .catch(error => throwException(CODES.AUTH.WRONG_CREDENTIALS, { reason: error.message }));
 
         if (!payload) throwException(CODES.AUTH.NO_TOKEN_PAYLOAD);
 
         return payload;
     }
 
-    public static async getAll(request): Promise<IAuthInfo> {
-        if (!request) return null;
+    public static getAll(request): IAuthInfo {
+        if (!request || !request.headers) return null;
 
         const authHeader = request.headers.authorization;
         if (!authHeader) return null;
@@ -39,26 +39,26 @@ export class AuthInfo {
         const token = authHeader.split(' ')?.[1];
         if (!token) return null;
 
-        return new JwtService({}).decode(token);
+        return jwtDecode(token);
     }
 
-    public static async getByName (request, fieldName: EAuthInfo) {
-        return (await this.getAll(request))?.[fieldName];
+    public static getByName(request, fieldName: EAuthInfo): any {
+        return this.getAll(request)?.[fieldName];
     }
 
-    public static async getByNames (request, fieldNames: Array<EAuthInfo>): Promise<Array<any>> {
+    public static getByNames(request, fieldNames: Array<EAuthInfo>): Array<any> {
         if (isHasEmpty(request, fieldNames)) return [];
 
-        const authPayload = await this.getAll(request.headers.authorization);
+        const authPayload = this.getAll(request.headers.authorization);
 
         return fieldNames.map(fieldName => authPayload[fieldName]);
     }
-    
-    public static async getUserId(request): Promise<string> {
+
+    public static getUserId(request): string {
         return this.getByName(request, EAuthInfo.userId);
     }
-    
-    public static async getDomain(request): Promise<string> {
+
+    public static getDomain(request): string {
         return this.getByName(request, EAuthInfo.domain);
     }
 }
